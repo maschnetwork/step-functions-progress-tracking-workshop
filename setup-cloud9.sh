f7dc5a3319330d0e415646d187c2f7388bec3fad
Maximilian Schellhorn <mxschell@amazon.de>
2023-03-08 15:30:21 +0100

    Added Cloud9 bootstrap

---
+++ setup-cloud9.sh
@@ -0,0 +1,24 @@
+## go to tmp directory
+cd /tmp
+
+## Ensure AWS CLI v2 is installed
+sudo yum -y remove aws-cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+
+# Install newer version of AWS SAM CLI
+wget -q https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip -q aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install --update
+rm -rf ./sam-installation/
+rm ./aws-sam-cli-linux-x86_64.zip
+
+## Install additional dependencies
+sudo yum install -y jq
+npm install -g aws-cdk --force
+npm install -g artillery
+
+## Resize disk
+/home/ec2-user/environment/aws-lambda-java-workshop/resize-cloud9.sh 30
